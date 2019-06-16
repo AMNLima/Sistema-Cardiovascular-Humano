46fafc2f18240c8f575a8d9cbf35a34878ad0f82
lfelipev <felipemmha@ic.ufal.br>
2019-06-15 21:57:26 -0300

    Modelo proposto por Simaan.

---
+++ runkut4.m
@@ -0,0 +1,19 @@
+function xf = runkut4(Ts,x,A,B,u)
+
+% Equação do distema
+xdot  = A*x + B*u;
+kx1  = Ts*xdot;
+x1  = x  + 0.5*kx1;
+
+xdot = A*x1 + B*u;
+kx2 = Ts*xdot;
+x1 = x + 0.5*kx2;
+
+xdot = A*x1 + B*u;
+kx3 = Ts*xdot;
+x1 = x + kx3;
+
+xdot = A*x1 + B*u;
+kx4 = Ts*xdot;
+
+xf = x + (kx1 + 2*kx2 + 2*kx3 + kx4)/6;
