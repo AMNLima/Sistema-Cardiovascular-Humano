9c43d4f2dc8ad185c6b01b3291e6e48827db1fb0
lfelipev <felipemmha@ic.ufal.br>
2019-08-04 15:17:40 -0300

    Simaan + ECG + LVAD

---
+++ Simaan+LVAD/changeDiodes.m
@@ -0,0 +1,64 @@
+function [A, B] = changeDiodes(Pao, Pae, Pve, E, omega, Vve)
+
+if(Pae > Pve)
+    Da = 0;
+    Dm = 1;
+elseif Pve > Pao
+    Da = 1;
+    Dm = 0;
+else
+    Da = 0;
+    Dm = 0;
+end
+
+if Vve > 1
+    Rk = 0;
+else
+    Rk = -3.5*(Vve-1);
+end
+
+% Resistances
+Rs = 1; % Sistemic Vascular
+Rm = 0.0050; % Mitral Valve
+Ra = 0.0010; % Aortic Valve
+Rc = 0.0398; % Characteristic
+Ri = 0.0677; % Inlet Resistance of Cannulae
+Ro = 0.0677; % Outlet Resistance of Cannulae
+
+% Compliances
+Cr = 4.400; % Left atrial
+Cs = 1.330; % Systemic
+Ca = 0.080; % Aortic
+Cae = Cr;
+Cao = Ca;
+
+% Inertance
+Ls = 0.0005; % Inertance of blood in Aorta
+L = Ls;
+Li = 0.0127;
+Lo = 0.0127;
+
+% Pressure Difference Parameters
+beta0 = 0.17070;
+beta1 = 0.02177;
+beta2 = -9.3e-5;
+
+V0 = 10; % Volume do ventrículo em ml
+
+%x = [Vve Pao Qa Ps Pae Qb];
+A = [ -((Dm/Rm) + (Da/Ra))*E      Da/Ra                     0           0           Dm/Rm                         -1;
+      Da/(Ra*Cao)*E               -(Da/(Ra*Cao))            (-1/Cao)    0           0                             1/Cao;
+      0                           1/L                       -Rc/L       -1/L        0                             0;
+      0                           0                         1/Cs        -1/(Rs*Cs)  1/(Rs*Cs)                     0;
+      Dm/(Rm*Cae)*E               0                         0           1/(Rs*Cae)  (-1/Cae)*((1/Rs) + (Dm/Rm))   0;
+      E/(Li + Lo + beta1)         -1/(Li + Lo + beta1)      0           0           0                             -(beta0 + Ri + Rk + Ro)/(Li + Lo + beta1)];
+
+%x = [Vve Pao Qa Ps Pae Qb]';
+B = [   ((Dm/Rm) + (Da/Ra))*V0;
+        (-Da*V0)/(Ra*Cao);
+        0;
+        0;
+        -(Dm*V0)/(Rm*Cae);
+        -((E*V0)/(Li + Lo + beta1)) - ((beta2*(omega*2*pi/60)^2)/(Li + Lo + beta1))];
+end
+
---
+++ Simaan+LVAD/coracao_balao.m
@@ -0,0 +1,76 @@
+% Parametros
+clc
+clear
+
+% Definição do tempo
+Ts = 0.0001;
+t = 0:Ts:60;
+
+% Função Elastança
+HR = 75;
+Emax = 2;
+Emin = 0.06;
+T = 60/HR;
+Tmax = 0.2 + 0.15*T;
+
+tn = (mod(t,T))/Tmax;
+En = 1.55 * (((tn./0.7).^1.9)./(1+(tn./0.7).^1.9)) .* (1./(1+(tn./1.17).^21.9));
+E = (Emax - Emin).*En + Emin;
+
+omega = 12000 + t.*100;
+
+%plot(E)
+%% Equação do sistema
+
+Vve = zeros(1, length(t));
+Pao = zeros(1, length(t));
+Qa = zeros(1, length(t));
+Ps = zeros(1, length(t));
+Pae = zeros(1, length(t));
+Pve = zeros(1, length(t));
+Qb = zeros(1, length(t));
+
+%x = [Vve Pao Qa Ps Pae Qb];
+x = [140 90 0 90 5 0]'; % Valores iniciais
+V0 = 10;
+
+for i=1:length(t)-1
+    Pve(i) = E(i)*(Vve(i)-V0); % Pressão no ventrículo esquerdo
+    
+    [A, B] = changeDiodes(Pao(i), Pae(i), Pve(i), E(i), omega(i)/1000, Vve(i)); % Função diodo
+    
+    x = runkut4(Ts, x, A, B, E(i)); % Runge-Kuta 4 ordem
+    
+    Vve(i+1) = x(1);
+    Pao(i+1) = x(2);
+    Qa(i+1) = x(3);
+    Ps(i+1) = x(4);
+    Pae(i+1) = x(5);
+    Qb(i+1) = x(6);
+end
+
+%% Plota tudo
+
+figure(1)
+
+% subplot(3, 1, 1);
+% plot(t, Pao, 'r', t, Pae, 'g', t, Pve, 'b')
+% legend('Pao','Pae', 'Pve')
+% grid on
+% title('Simulação do modelo 0D do sistema cardiovascular humano')
+% ylabel('Pressão (mmHg)')
+% xlabel('tempo (s)')
+% 
+% subplot(3, 1, 2);
+% plot(t, Qa, 'm')
+% grid on
+% ylabel('Fluxo (m/s)')
+% xlabel('tempo (s)')
+% title('Fluxo Aórtico')
+
+subplot(1, 1, 1);
+plot(t, Qb, 'r')
+grid on
+ylabel('Volume (ml)')
+xlabel('tempo (s)')
+title('Volume no ventrículo esquerdo')
